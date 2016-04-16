ef73cb5b9a5e21e4fff0754821470346369f577c
Andrea <lottarini@cs.columbia.edu>
2016-04-15 20:55:02 -0400

    script for synthesis on condor

--- env_variables.sh
+++ env_variables.sh
@@ -11,7 +11,13 @@ fi
 export CONNECT_GENERATOR=/proj/arcade/home/lottarini/connect_generator/connect_gen_network
 
 if [ ${#CONNECT_GENERATOR} -eq 0 ] || [ ! -f $CONNECT_GENERATOR ]; then
-	1>&2 echo "CONNECT GENERATOR does not point to a valid directory - please fix" 
+	1>&2 echo "CONNECT GENERATOR does not point to a valid file - please fix" 
+fi
+
+export ASSIGNMENT_GENERATOR=/proj/arcade/home/lottarini/connect_tester/assignments.py
+
+if [ ${#ASSIGNMENT_GENERATOR} -eq 0 ] || [ ! -f $ASSIGNMENT_GENERATOR ]; then
+	1>&2 echo "CONNECT GENERATOR does not point to a valid file - please fix" 
 fi
 
 
